b30def6949e48d44f45ff5cc9d3e53715fc12de9
nrifath <nrifath@yahoo.com>
2015-03-06 09:34:59 +0600

    Initial Commit

---
+++ Looping/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Looping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            int count = 0;
+            do
+            {
+
+                Console.WriteLine(count);
+                count++;
+            } while (count < 10);
+
+
+
+            //int count = 0;
+            //while (count<10)
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+            Console.ReadKey();
+        }
+    }
+}
